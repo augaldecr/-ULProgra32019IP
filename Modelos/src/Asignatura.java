30b5f025f4f6111abcd5c8e7793b8bba55f53d21
augaldecr <augaldecr@gmail.com>
2019-02-01 18:59:09 -0600

    Added Asignatura

---
+++ Modelos/src/Asignatura.java
@@ -0,0 +1,24 @@
+public class Asignatura {
+    
+    private int ID;
+    private String Nombre;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    
+}
