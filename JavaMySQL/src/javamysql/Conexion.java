63448a544c0e4470c27e9737a132c38fea40e0cd
augal <augal@DESKTOP-AK09J7P>
2019-02-25 15:22:55 -0600

    Conexion funcionando

--- JavaMySQL/src/javamysql/Conexion.java
+++ JavaMySQL/src/javamysql/Conexion.java
@@ -7,17 +7,12 @@ import java.sql.SQLException;
 public class Conexion {
     
     private static Connection conn;
-    //private static final String driver ="com.mysql.cj.jdbc.Driver";
-    private static final String usuario ="Vegetta777";
-    private static final String contrasenna ="VivaWillyreh3";
-    private static final String host ="jdbc:mysql://localhost:3309/?";
-    private static final String db = "db_estudiantes";
 
     public Conexion() {
         conn = null;
         try {
-            //Class.forName(driver);
-            conn = DriverManager.getConnection("jdbc:mysql://(host=localhost,port=3309)/db_estudiantes?user=Vegetta777&password=VivaWillyreh3");
+            conn = DriverManager.getConnection("jdbc:mysql://(host=localhost,port=3306)/db_estudiantes?"
+                    + "user=Vegetta777&password=VivaWillyreh3");
             if (conn != null) {
                 System.out.println("Conexion establecida");
             }
